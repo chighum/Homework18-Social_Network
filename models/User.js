63c96b81fadcec225ba474d3a5e4b6827a453848
Charlie Highum <chighum41@gmail.com>
2022-01-12 16:33:19 -0700

    wrote User.js model including the virtual property to get friendCount

--- models/User.js
+++ models/User.js
@@ -1,1 +1,42 @@
-const { Schema, model}
+const { Schema, model } = require("mongoose");
+const Thoughts = require("./Thoughts");
+const Friends = require("./Friends");
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "User email required"],
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+        },
+        message: (data) => `${data.value} is not a valid email!`,
+      },
+    },
+    thoughts: [Thoughts],
+    friends: [Friends],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// virtual property to get length of friends array and store it in a variable called friendCount
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
