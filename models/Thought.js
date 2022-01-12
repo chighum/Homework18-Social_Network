02dd3a7ecbcc7b4db9c7067a3cb061d7c11da056
Charlie Highum <chighum41@gmail.com>
2022-01-12 16:41:53 -0700

    wrote Thought.js file aside from createdAt column with current timestamp

---
+++ models/Thought.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+
+const reactionSchema = new Schema({
+  reactionId: {},
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+  },
+});
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactions],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thoughts = model("thoughts", thoughtSchema);
+
+module.exports = Thoughts;
