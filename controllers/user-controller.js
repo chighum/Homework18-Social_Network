a91d2ffc7d6e34b25569b5958bf359adcc82b8d0
Charlie Highum <chighum41@gmail.com>
2022-01-12 17:11:52 -0700

    wrote functionality to getAllUsers, GetOneUser, postUser, and deleteUser in user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,15 +1,47 @@
-// GET all users
+const { User, Thought } = require("../models");
 
-// GET a single user by its _id and populated thought and friend data
+module.exports = {
+  // GET all users
+  getAllUsers(req, res) {
+    User.find()
+      .then((users) => res.status(200).json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  // GET a single user by its _id and populated thought and friend data
+  getOneUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.status(200).json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // POST a new user
+  postUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
 
-// POST a new user
+  // PUT to update a user by its _id
 
-// PUT to update a user by its _id
+  // DELETE to remove user by its _id
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : Application.deleteMany({ _id: { $in: user.applications } })
+      )
+      .then(() => res.json({ message: "User and associated apps deleted!" }))
+      .catch((err) => res.status(500).json(err));
+  },
 
-// DELETE to remove user by its _id
+  // /api/users/:userId/friends/:friendId
 
-// /api/users/:userId/friends/:friendId
+  // POST a new friend to a user's friend list
 
-// POST a new friend to a user's friend list
-
-// DELETE to remove a friend from a user's friend list
+  // DELETE to remove a friend from a user's friend list
+};
